0e59c4a893554edbb6b2b1c878aa8d509662271f
alifislam724github <alifislam724@gmail.com>
2022-05-27 02:20:29 +0600

    CV task

---
+++ CV task/CV task/Controllers/CVController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV_task.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult PersonalDetails()
+        {
+            return View();
+        }
+
+    }
+
+}
---
+++ CV task/CV task/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CV_task.MvcApplication" Language="C#" %>
